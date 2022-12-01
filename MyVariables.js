b74dfbb0744a7988654fa27e8b811cf707b985a8
John Wight <jwight@adobe.com>
2022-12-01 10:11:24 -0700

    training

---
+++ MyVariables.js
@@ -0,0 +1,9 @@
+var s_account = "adobecx1anythingyoulike";
+var s = s_gi(s_account);
+
+s.trackingServer = "john.data.adobedc.net";
+s.pageName = "Russ's home page";
+s.eVar1 = "Example value";
+s.prop1 = "Prop";
+
+s.t();
