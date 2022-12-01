281e91582f350081b5f632ea8218f4994a5cd470
John Wight <jwight@adobe.com>
2022-12-01 10:16:02 -0700

    Update MyVariables.js

--- MyVariables.js
+++ MyVariables.js
@@ -2,7 +2,7 @@ var s_account = "adobecx1anythingyoulike";
 var s = s_gi(s_account);
 
 s.trackingServer = "john.data.adobedc.net";
-s.pageName = "Russ's home page";
+s.pageName = "John's home page";
 s.eVar1 = "Example value";
 s.prop1 = "Prop";
 
